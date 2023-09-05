a4942cf01985db5b6833baac203c36cc50ff6be8
Pathricc1234 <abelputra101@gmail.com>
2023-09-05 16:34:23 +0700

    First Commit

---
+++ Tes_Git.java
@@ -0,0 +1,15 @@
+public class Tes_Git{
+    public static void main(String[] args){
+        int num = 9;
+        int add = 6;
+        System.out.println("Start");
+        if(add <= 10){
+            num = num + add;
+            System.out.println(num);
+        }
+        else{
+            System.out.println(num);
+        }
+        System.out.println("Done");
+    }
+}
